21082bcde239b0ca7ed0a53b15583e2da30bb6d5
Avinash <Avinashts1998@gmail.com>
2021-09-25 05:22:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="headline">Congradulations</h1>
+    <div className="card-contaner">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="person-image"
+      />
+      <h1>Kiran V</h1>
+      <p>Vishnu Institution for computer education and technology</p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        className="person-image"
+      />
+      >
+    </div>
+    >
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
